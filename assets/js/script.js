05170f244e6b04ed3d589bd3ec4e67f78217c1a6
EsdrasMOS <esdras.mosilva@gmail.com>
2025-09-29 17:04:22 -0300

    Ajuste do pulo do Mario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,31 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 
-const jump = ()=>{
+const jump = () => {
     mario.classList.add("jump");
 
-    setTimeout(()=>{
-
+    setTimeout(() => {
         mario.classList.remove("jump");
-
     }, 500);
 }
 
-const loop= setInterval(()=>{
-
-    console.log("loop")
-
+const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = window.getComputedStyle(mario).bottom.replace("px", "");
-    
-    console.log(marioPosition)
+    const marioPosition = parseInt(window.getComputedStyle(mario).bottom.replace("px", ""));
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
-        pipe.style.animation='none';
-        pipe.style.left=`${pipePosition}px`;
-
-        pipe.style.animation='none';
-        mario.style.left=`${marioPosition}px`;
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
 
         mario.src = "./assets/img/game-over.png";
         mario.style.width = "75px";
-        mario.style.marginLeft ="50px";
+        mario.style.marginLeft = "50px";
 
         clearInterval(loop);
-
     }
-},10)
+}, 10);
 
-document.addEventListener("keydown", jump);
+document.addEventListener("keyup", jump);
